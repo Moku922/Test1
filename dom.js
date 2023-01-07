0306e76d0f7390acce079616df14d0b62e733b0c
Moku922 <abdulmoqtadir1@gmail.com>
2023-01-07 22:17:51 +0530

    Add file

---
+++ dom.js
@@ -0,0 +1,12 @@
+// console.log(document.head)
+// console.log(document.body)
+// console.log(document.)
+let headerTitle = (document.getElementById("header-title"));
+headerTitle.innerText = "Hello"
+
+headerTitle.style.border = "solid 3px #000"
+
+let title = document.getElementsByClassName("title")[0];
+console.log(title);
+title.style.color = "green"
+title.style.fontWeight="900"
